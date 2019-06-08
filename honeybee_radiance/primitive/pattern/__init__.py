a46f9aea73f29cd1839de0d9d06db0e80889672e
Mostapha <sadeghipour@gmail.com>
2019-06-07 21:38:02 -0400

    feat(primitive): add support for all radiance primitives
    This commit adds support for all radiance primitive types: https://floyd.lbl.gov/radiance/refer/ray.html#Primitive

---
+++ honeybee_radiance/primitive/pattern/__init__.py
@@ -0,0 +1,14 @@
+"""Radiance Patterns.
+
+Patterns are used to modify the reflectance of materials.
+
+See http://radsite.lbl.gov/radiance/refer/ray.html#Patterns
+"""
+
+from .brightdata import Brightdata
+from .brightfunc import Brightfunc
+from .brighttext import Brighttext
+from .colordata import Colordata
+from .colorfunc import Colorfunc
+from .colorpict import Colorpict
+from .colortext import Colortext
---
+++ honeybee_radiance/primitive/texture/__init__.py
@@ -0,0 +1,7 @@
+"""Radiance Textures.
+
+A texture is a perturbation of the surface normal, and is given by either a function or
+data.
+"""
+from .texdata import Texdata
+from .texfunc import Texfunc
