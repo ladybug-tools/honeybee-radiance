c8c76f9f61f881eb0adaca28c4f79d80626f4f7b
Mostapha <sadeghipour@gmail.com>
2019-06-25 17:09:47 -0400

    feat(options): add RtraceOptions class
    This is the first of many command options to be added to honeybee_radiance.
    
    I wrote a function that generates a placeholder file from radiance defaults command output. It proved to be very useful so I'm including it in this commit as `_optutil.py` to be used in the future for other commands if applicable.

---
+++ tests/rtrace_options_test.py
@@ -0,0 +1,87 @@
+"""Test rtrace options."""
+from honeybee_radiance.command.options.rtrace import RtraceOptions
+import pytest
+
+
+def test_default():
+    options = RtraceOptions()
+    assert options.to_radiance() == ''
+
+
+def test_assignment():
+    options = RtraceOptions()
+    options.ab = 5
+    assert options.ab == 5
+    assert options.to_radiance() == '-ab 5'
+    # handle wrong value type like a boss
+    options.ab = 3.2
+    assert options.ab == 3
+    assert options.to_radiance() == '-ab 3'
+
+
+def test_reassignment():
+    options = RtraceOptions()
+    options.ab = 5
+    assert options.ab == 5
+    assert options.to_radiance() == '-ab 5'
+    # remove assigned values
+    options.ab = None
+    assert options.ab == None
+    assert options.to_radiance() == ''
+
+
+def test_multiple_assignment():
+    options = RtraceOptions()
+    options.ab = 2
+    options.ad = 546
+    assert '-ab 2' in options.to_radiance()
+    assert 'ad 546' in options.to_radiance()
+
+
+def test_boolean_assignment():
+    options = RtraceOptions()
+    options.w = False
+    options.h = True
+    assert '-w-' in options.to_radiance()
+    assert '-h+' in options.to_radiance()
+
+
+def test_invalid_assignment():
+    opts = RtraceOptions()
+    with pytest.raises(AttributeError):
+        opts.mm = 20
+    
+    with pytest.raises(ValueError):
+        opts.ab = 'ambient bounces'  # must be a numeric value
+
+
+def test_exclusives_i():
+    opt = RtraceOptions()
+    opt.i = True
+    with pytest.raises(AssertionError):
+        # i and I cannot be used together
+        opt.I = True
+
+
+def test_exclusives_ae():
+    opt = RtraceOptions()
+    opt.ae = 'modifier_1'
+    with pytest.raises(AssertionError):
+        # i and I cannot be used together
+        opt.ai = 'modifier_2'
+
+
+def test_from_string_non_standard():
+    opt = RtraceOptions()
+    opt_str = '-g 200'
+    opt.update_from_string(opt_str)
+    assert '-g 200' in opt.to_radiance()
+
+def test_from_string():
+    opt = RtraceOptions()
+    opt_str = '-I -u- -h+ -lw 2e-06'
+    opt.update_from_string(opt_str)
+    assert opt.I == True
+    assert opt.u == False
+    assert opt.h == True
+    assert opt.lw == 2e-06
