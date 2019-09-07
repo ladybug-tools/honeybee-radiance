cfe4d4419968886a01d233a8190e12c54e9b2386
Mostapha <sadeghipour@gmail.com>
2019-09-07 15:07:03 -0400

    test(cli): add tests for grid and sky

---
+++ tests/cli/sky_cli_test.py
@@ -0,0 +1,31 @@
+"""Test sky subcommands."""
+
+from click.testing import CliRunner
+from honeybee_radiance.cli.sky import sky_with_certain_illum
+import uuid
+import os
+
+
+def test_illuminance_sky():
+    name = str(uuid.uuid4()) + '.sky'
+    folder = './tests/assets/temp'
+    runner = CliRunner()
+    result = runner.invoke(
+        sky_with_certain_illum,
+        ['100000', '--folder', folder, '--name', name]
+    )
+    assert result.exit_code == 0
+    # check the file is created
+    assert os.path.isfile(os.path.join(folder, name))
+
+
+def test_illuminance_sky_fail():
+    name = str(uuid.uuid4()) + '.sky'
+    folder = './tests/assets/temp'
+    runner = CliRunner()
+    result = runner.invoke(
+        sky_with_certain_illum,
+        ['c', '--folder', folder, '--name', name]
+    )
+    # input error
+    assert result.exit_code == 2
