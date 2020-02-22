4612fbec7a894da7619c8e89667512cbaa37e417
sg <sgaray1970@gmail.com>
2020-02-21 21:55:30 -0500

    doc(materials): Add Arguments & Properties missing in some class docstrings

--- honeybee_radiance/modifier/material/metfunc.py
+++ honeybee_radiance/modifier/material/metfunc.py
@@ -11,6 +11,28 @@ class Metfunc(Material):
 
     Metfunc is identical to plasfunc and takes the same arguments, but the specular
     component is multiplied also by the material color.
+
+    Args:
+        name: Primitive name as a string. Cannot contain white spaces or special
+            characters.
+        modifier: Modifier. It can be primitive, mixture, texture or pattern.
+            (Default: "void").
+        values: An array 3 arrays for primitive data. Each of the 3 sub-arrays
+            refer to a line number in the radiance primitve definitions and the
+            values in each array correspond to values occurring within each line.
+        is_opaque: A boolean to indicate whether this primitive is opaque.
+        dependencies: A list of primitives that this primitive depends on. This
+            argument is only useful for defining advanced primitives that are
+            defined based on other primitives. (Default: []).
+
+    Properties:
+        * name
+        * values
+        * modifier
+        * dependencies
+        * is_modifier
+        * is_material
+        * is_opaque
     """
     __slots__ = ()
 
