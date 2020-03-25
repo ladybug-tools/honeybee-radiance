5807ccae94e9ecdd568cb9cab4af734143c57553
Antoine Dao <antoinedao1@gmail.com>
2020-03-25 12:18:02 +1000

    fix(build): add honeybee-core[cli] to `extras_require`

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setuptools.setup(
     include_package_data=True,
     install_requires=requirements,
     extras_require={
-        'cli': ['click>=5.1']
+        'cli': ['click>=5.1', 'honeybee-core[cli]>=1.10.0']
     },
     entry_points={
         "console_scripts": ["honeybee-radiance = honeybee_radiance.cli:radiance"]
