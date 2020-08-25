29597551fa61d024689e4e1aa44f4108db7bb5ca
Mostapha <sadeghipour@gmail.com>
2020-08-25 09:07:58 -0400

    fix(sky cli): add skydome to cli commands

--- honeybee_radiance/cli/sky.py
+++ honeybee_radiance/cli/sky.py
@@ -34,3 +34,19 @@ def sky_with_certain_illum(illum, folder, name):
     except Exception:
         _logger.exception('Failed to generate sky.')
         sys.exit(1)
+
+
+@sky.command('skydome')
+@click.option('--folder', help='Output folder.', default='.', show_default=True)
+@click.option('--name', help='Sky file name.', default=None, show_default=True)
+def sky_dome(folder, name):
+    """Virtual skydome for daylight coefficient studies with constant radiance.
+
+    Use this sky to calculate daylight matrix.
+    """
+    try:
+        c_sky = hbsky.SkyDome()
+        c_sky.to_file(folder, name, True)
+    except Exception:
+        _logger.exception('Failed to generate sky.')
+        sys.exit(1)
