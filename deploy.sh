15b0be6319ef2a0e9cf857fd631fc79ad9d09e3b
Tyler Kvochick <tymokvo@gmail.com>
2021-02-24 14:38:52 -0800

    Acquire radiance.zip in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -14,15 +14,20 @@ echo "PyPi Deployment..."
 echo "Building distribution"
 python setup.py sdist bdist_wheel
 echo "Pushing new version to PyPi"
-twine upload dist/* -u $PYPI_USERNAME -p $PYPI_PASSWORD 
+twine upload dist/* -u $PYPI_USERNAME -p $PYPI_PASSWORD
 
+export radiance_version='5.3a.fc2a2610'
+
+curl -L "https://ladybug-tools-releases.nyc3.digitaloceanspaces.com/Radiance_${radiance_version}_Linux.zip" --output radiance.zip
+
+unzip -p radiance.zip | tar xz
 
 echo "Docker Deployment..."
 echo "Login to Docker"
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-docker build . -t $CONTAINER_NAME:$NEXT_RELEASE_VERSION 
+docker build . -t $CONTAINER_NAME:$NEXT_RELEASE_VERSION --build-arg radiance_version=${radiance_version}
 docker tag $CONTAINER_NAME:$NEXT_RELEASE_VERSION $CONTAINER_NAME:latest
 
 docker push $CONTAINER_NAME:latest
-docker push $CONTAINER_NAME:$NEXT_RELEASE_VERSION 
+docker push $CONTAINER_NAME:$NEXT_RELEASE_VERSION
