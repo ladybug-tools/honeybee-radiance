c3bc16b46a0bf444accd9fbc07ecdf850ddea246
Mostapha <sadeghipour@gmail.com>
2021-03-17 21:03:17 -0400

    feat(postprocess): improve postporcess commands
    - added average and count
    - made to binary more flexible

--- honeybee_radiance/cli/postprocess.py
+++ honeybee_radiance/cli/postprocess.py
@@ -4,6 +4,7 @@ import sys
 import os
 import logging
 from honeybee_radiance.postprocess.annualdaylight import metrics_to_folder
+from honeybee_radiance.cli.util import get_compare_func, remove_header
 
 _logger = logging.getLogger(__name__)
 
@@ -21,7 +22,30 @@ def post_process():
     '--output', '-o', help='Optional path to output file to output the name of the newly'
     ' created matrix. By default the list will be printed out to stdout',
     type=click.File('w'), default='-')
-def convert_matrix_to_binary(input_matrix, output):
+@click.option(
+    '--minimum', type=float, default='-inf', help='Minimum range for values to be '
+    'converted to 1.'
+)
+@click.option(
+    '--maximum', type=float, default='+inf', help='Maximum range for values to be '
+    'converted to 1.'
+)
+@click.option(
+    '--include-max/--exclude-max', is_flag=True, help='A flag to include the maximum '
+    'threshold itself. By default the threshold value will be included.', default=True
+)
+@click.option(
+    '--include-min/--exclude-min', is_flag=True, help='A flag to include the minimum '
+    'threshold itself. By default the threshold value will be included.', default=True
+)
+@click.option(
+    '--comply/--reverse', is_flag=True, help='A flag to reverse the selection logic. '
+    'This is useful for cases that you want to all the values outside a certain range '
+    'to be converted to 1. By default the input logic will be used as is.', default=True
+)
+def convert_matrix_to_binary(
+    input_matrix, output, minimum, maximum, include_max, include_min, comply
+        ):
     """Postprocess a Radiance matrix and convert it to 0-1 values.
 
     \b
@@ -30,32 +54,96 @@ def convert_matrix_to_binary(input_matrix, output):
     ignored.
 
     """
+
+    compare = get_compare_func(include_min, include_max, comply)
+    minimum = float(minimum)
+    maximum = float(maximum)
     try:
-        with open(input_matrix) as inf:
-            first_line = next(inf)
-            if first_line[:10] == '#?RADIANCE':
-                for line in inf:
-                    if line[:7] == 'FORMAT=':
-                        # pass next empty line
-                        next(inf)
-                        break
-                    continue
-            else:
-                values = [
-                    '0' if float(v) == 0 else '1' for v in first_line.split()
-                ]
-                output.write('\t'.join(values) + '\n')
-            for line in inf:
-                # write binary values to new file
-                values = [
-                    '0' if float(v) == 0 else '1' for v in line.split()
-                ]
-                output.write('\t'.join(values) + '\n')
+        first_line, input_file = remove_header(input_matrix)
+        values = [
+            '1' if compare(float(v), minimum, maximum) else '0'
+            for v in first_line.split()
+        ]
+        output.write('\t'.join(values) + '\n')
+        for line in input_file:
+            # write binary values to new file
+            values = [
+                '1' if compare(float(v), minimum, maximum) else '0'
+                for v in line.split()
+            ]
+            output.write('\t'.join(values) + '\n')
     except Exception:
         _logger.exception('Failed to convert the input file to binary format.')
         sys.exit(1)
     else:
         sys.exit(0)
+    finally:
+        input_file.close()
+
+
+@post_process.command('count')
+@click.argument(
+    'input-matrix', type=click.Path(exists=True, file_okay=True, resolve_path=True)
+)
+@click.option(
+    '--output', '-o', help='Optional path to output file to output the name of the newly'
+    ' created matrix. By default the list will be printed out to stdout',
+    type=click.File('w'), default='-')
+@click.option(
+    '--minimum', type=float, default='-inf', help='Minimum range for values to be '
+    'converted to 1.'
+)
+@click.option(
+    '--maximum', type=float, default='+inf', help='Maximum range for values to be '
+    'converted to 1.'
+)
+@click.option(
+    '--include-max/--exclude-max', is_flag=True, help='A flag to include the maximum '
+    'threshold itself. By default the threshold value will be included.', default=True
+)
+@click.option(
+    '--include-min/--exclude-min', is_flag=True, help='A flag to include the minimum '
+    'threshold itself. By default the threshold value will be included.', default=True
+)
+@click.option(
+    '--comply/--reverse', is_flag=True, help='A flag to reverse the selection logic. '
+    'This is useful for cases that you want to all the values outside a certain range '
+    'to be converted to 1. By default the input logic will be used as is.', default=True
+)
+def count_values(
+    input_matrix, output, minimum, maximum, include_max, include_min, comply
+        ):
+    """Count values in a row that meet a certain criteria.
+
+    \b
+    This command is useful for post processing results like the number of sensors 
+    which receive more than X lux at any timestep.
+
+    """
+    compare = get_compare_func(include_min, include_max, comply)
+    minimum = float(minimum)
+    maximum = float(maximum)
+    try:
+        first_line, input_file = remove_header(input_matrix)
+        value = sum(
+            1 if compare(float(v), minimum, maximum) else 0
+            for v in first_line.split()
+        )
+        output.write('%d\n' % value)
+        for line in input_file:
+            # write binary values to new file
+            value = sum(
+                1 if compare(float(v), minimum, maximum) else 0
+                for v in line.split()
+            )
+            output.write('%d\n' % value)
+    except Exception:
+        _logger.exception('Failed to convert the input file to binary format.')
+        sys.exit(1)
+    else:
+        sys.exit(0)
+    finally:
+        input_file.close()
 
 
 @post_process.command('sum-row')
@@ -76,27 +164,60 @@ def sum_matrix_rows(input_matrix, output):
 
     """
     try:
-        with open(input_matrix) as inf:
-            first_line = next(inf)
-            if first_line[:10] == '#?RADIANCE':
-                for line in inf:
-                    if line[:7] == 'FORMAT=':
-                        # pass next empty line
-                        next(inf)
-                        break
-                    continue
-            else:
-                value = sum(float(v) for v in first_line.split())
-                output.write('%s\n' % value)
-            for line in inf:
-                # write sum to a new file
-                value = sum(float(v) for v in line.split())
-                output.write('%s\n' % value)
+        first_line, input_file = remove_header(input_matrix)
+        value = sum(float(v) for v in first_line.split())
+        output.write('%s\n' % value)
+        for line in input_file:
+            # write sum to a new file
+            value = sum(float(v) for v in line.split())
+            output.write('%s\n' % value)
     except Exception:
-        _logger.exception('Failed to convert the input file to binary format.')
+        _logger.exception('Failed to sum numbers in each row.')
         sys.exit(1)
     else:
         sys.exit(0)
+    finally:
+        input_file.close()
+
+
+@post_process.command('average-row')
+@click.argument(
+    'input-matrix', type=click.Path(exists=True, file_okay=True, resolve_path=True)
+)
+@click.option(
+    '--output', '-o', help='Optional path to output file to output the name of the newly'
+    ' created matrix. By default the list will be printed out to stdout',
+    type=click.File('w'), default='-')
+def average_matrix_rows(input_matrix, output):
+    """Postprocess a Radiance matrix and average the numbers in each row.
+
+    \b
+    This command is useful for translating Radiance results to outputs like radiation
+    to average radiation. Input matrix must be in ASCII format. The header in the input
+    file will be ignored.
+
+    """
+    try:
+        first_line, input_file = remove_header(input_matrix)
+
+        # calculate the values for the first line
+        values = [float(v) for v in first_line.split()]
+        count = len(values)
+        output.write('%s\n' % sum(values) / count)
+
+        # write rest of the lines
+        for line in input_file:
+            # write sum to a new file
+            value = sum(float(v) for v in line.split())
+            output.write('%s\n' % value / count)
+
+    except Exception:
+        _logger.exception('Failed to average the numbers in each row.')
+        sys.exit(1)
+    else:
+        sys.exit(0)
+    finally:
+        input_file.close()
 
 
 @post_process.command('annual-daylight')
