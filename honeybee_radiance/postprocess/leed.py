aff899f375031692ad2b1d8b92b0cd45f21a1cb9
Chris Mackey <chris@ladybug.tools>
2021-11-01 20:00:28 -0400

    fix(leed): Add a CSV for space-by-space pass/fail information

--- honeybee_radiance/postprocess/leed.py
+++ honeybee_radiance/postprocess/leed.py
@@ -2,8 +2,10 @@
 import json
 import os
 import shutil
+import math
 
 from honeybee.model import Model
+from honeybee.units import conversion_factor_to_meters
 from ..writer import _filter_by_pattern
 
 
@@ -99,11 +101,81 @@ def _sum_passing_area(pass_fails, grid_areas):
     """Compute the sum of passing area given aligned pass_fail and grid_area matrices.
     """
     area_passing = 0
+    for pf, ga in zip(pass_fails, grid_areas):
+        if pf == 1:
+            area_passing += ga
+    return area_passing
+
+
+def _sum_all_passing_area(pass_fails, grid_areas):
+    """Compute the sum of passing area given aligned pass_fail and grid_area matrices.
+    """
+    area_passing = 0
     for p_fails, g_areas in zip(pass_fails, grid_areas):
         for pf, ga in zip(p_fails, g_areas):
             if pf == 1:
                 area_passing += ga
     return area_passing
+
+
+def _space_by_space_summary(
+        folder, sub_folder, grid_areas, units_conversion,
+        pass_fail_comb, pass_fail_9, pass_fail_3, filter_pattern):
+    """Write a CSV with space-by-space information for the percentage of sensors passing.
+
+    Args:
+        folder: Project folder for a LEED daylight illuminance simulation.
+        sub_folder: Relative path for a subfolder to write the pass/fail files for
+            each sensor grid.
+        grid_areas: A matrix of numbers for the area occupied by each sensor.
+        units_conversion: A number for the conversion factor from the grid_areas units
+            to Meters.
+        pass_fail_comb: Matrix of ones/zeros for combined pass/failing.
+        pass_fail_9: Matrix of ones/zeros for 9AM pass/failing.
+        pass_fail_3:  Matrix of ones/zeros for 3PM pass/failing.
+        filter_pattern: Pattern used to filter the grids.
+    """
+     # get the grids_info.json and determine which grids we are working with
+    res_folder_9 = os.path.join(folder, '9AM', 'results')
+    info_json = os.path.join(res_folder_9, 'grids_info.json')
+    with open(info_json) as data_f:
+        data = json.load(data_f)
+    grids = _filter_by_pattern(data, filter=filter_pattern)
+
+    # define the header row of the CSV
+    csv_data = [['Space Name', 'Sensor Count']]
+    if len(grid_areas) == len(pass_fail_9):  # compute passing floor area for each grid
+        csv_data[0].extend(['Area (m2)', 'Area (ft2)', 'Spacing (m)'])
+    csv_data[0].extend(['% Passing 9AM', '% Passing 3PM', '% Passing Combined'])
+
+    # loop through each grid and get the rows of the CSV
+    if len(grid_areas) == len(pass_fail_9):
+        all_data = zip(grids, grid_areas, pass_fail_comb, pass_fail_9, pass_fail_3)
+        for gr, gr_a, res_c, res_9, res_3 in all_data:
+            csv_row = [gr['full_id'], gr['count']]
+            total_a = sum(gr_a)
+            csv_row.append(round(total_a * units_conversion, 3))
+            csv_row.append(round(csv_row[2] / 0.305, 3))
+            csv_row.append(round(math.sqrt(csv_row[2] / csv_row[1]), 3))
+            csv_row.append(round(100 * (_sum_passing_area(res_9, gr_a) / total_a), 2))
+            csv_row.append(round(100 * (_sum_passing_area(res_3, gr_a) / total_a), 2))
+            csv_row.append(round(100 * (_sum_passing_area(res_c, gr_a) / total_a), 2))
+            csv_data.append(csv_row)
+    else:
+        all_data = zip(grids, pass_fail_comb, pass_fail_9, pass_fail_3)
+        for gr, res_c, res_9, res_3 in all_data:
+            csv_row = [gr['full_id'], gr['count']]
+            total_count = csv_row[1]
+            csv_row.append(round(100 * (sum(res_9) / total_count), 2))
+            csv_row.append(round(100 * (sum(res_3) / total_count), 2))
+            csv_row.append(round(100 * (sum(res_c) / total_count), 2))
+            csv_data.append(csv_row)
+
+    # write the results into a CSV
+    output_file = os.path.join(folder, sub_folder, 'space_summary.csv')
+    with open(output_file, 'w') as of:
+        for row in csv_data:
+            of.write(','.join((str(v) for v in row)) + '\n')
 
 
 def leed_illuminance_to_folder(
@@ -125,8 +197,8 @@ def leed_illuminance_to_folder(
         grids_filter: A pattern to filter the grids. By default all the grids will be
             processed.
         sub_folder: Relative path for a subfolder to write the pass/fail files for
-            each sensor grid. If None, the files will not be written and only
-            the summary dictionary will be calculated.
+            each sensor grid and a space-by-space summary CSV. If None, the files
+            will not be written and only the summary dictionary will be calculated.
 
     Returns:
         A dictionary with a summary of LEED credits in the format below. All
@@ -170,10 +242,25 @@ def leed_illuminance_to_folder(
                 p_fails_comb.append(0)
         pass_fail_comb.append(p_fails_comb)
 
+    # next, check to see if there is a HBJSON with sensor grid meshes for areas
+    grid_areas, units_conversion = [], 1
+    for base_file in os.listdir(folder):
+        if base_file.endswith('.hbjson') or base_file.endswith('.hbpkl'):
+            hb_model = Model.from_file(os.path.join(folder, base_file))
+            units_conversion = conversion_factor_to_meters(hb_model.units)
+            filt_grids = _filter_by_pattern(
+                hb_model.properties.radiance.sensor_grids, filter=grids_filter)
+            for s_grid in filt_grids:
+                if s_grid.mesh is not None:
+                    grid_areas.append(s_grid.mesh.face_areas)
+
     # write the pass/fail criteria into the sub-directory if specified
     if sub_folder:
         _pass_fail_to_files(
             folder, sub_folder, pass_fail_comb, pass_fail_9, pass_fail_3, grids_filter)
+        _space_by_space_summary(
+            folder, sub_folder, grid_areas, units_conversion,
+            pass_fail_comb, pass_fail_9, pass_fail_3, grids_filter)
 
     # setup the summary dictionary with the results
     summary_dict = {
@@ -183,22 +270,11 @@ def leed_illuminance_to_folder(
         'total_sensor_count': sum(len(pf9) for pf9 in pass_fail_9)
     }
 
-    # next, check to see if there is a HBJSON with sensor grid meshes for areas
-    grid_areas = []
-    for base_file in os.listdir(folder):
-        if base_file.endswith('.hbjson') or base_file.endswith('.hbpkl'):
-            hb_model = Model.from_file(os.path.join(folder, base_file))
-            filt_grids = _filter_by_pattern(
-                hb_model.properties.radiance.sensor_grids, filter=grids_filter)
-            for s_grid in filt_grids:
-                if s_grid.mesh is not None:
-                    grid_areas.append(s_grid.mesh.face_areas)
-
     # determine the percentage passing from either mesh areas or sensor counts
     if len(grid_areas) == len(pass_fail_9):  # compute passing floor area for each grid
-        area_pass_comb = _sum_passing_area(pass_fail_comb, grid_areas)
-        area_pass_9 = _sum_passing_area(pass_fail_9, grid_areas)
-        area_pass_3 = _sum_passing_area(pass_fail_3, grid_areas)
+        area_pass_comb = _sum_all_passing_area(pass_fail_comb, grid_areas)
+        area_pass_9 = _sum_all_passing_area(pass_fail_9, grid_areas)
+        area_pass_3 = _sum_all_passing_area(pass_fail_3, grid_areas)
         area_total = sum(sum(sar) for sar in grid_areas)
         summary_dict['floor_area_passing'] = area_pass_comb
         summary_dict['floor_area_passing_9AM'] = area_pass_9
