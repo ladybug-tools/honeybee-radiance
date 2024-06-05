97443d7f5abe37a989e97e16610d0bec5a827931
Mikkel Pedersen <mkklpdrsn@gmail.com>
2024-06-05 12:05:50 +0200

    docs(sky): Correct docs in ABNT NBR 15575 skies

--- honeybee_radiance/cli/sky.py
+++ honeybee_radiance/cli/sky.py
@@ -473,13 +473,13 @@ def leed_illuminance(wea, north, folder, name, log_file):
 @click.option(
     '--north', '-n', type=float, default=0, show_default=True, help='A '
     'number between -360 and 360 for the counterclockwise difference between '
-    'the North and the positive Y-axis in degrees. 90 is West; 270 is East')
+    'the North and the positive Y-axis in degrees. 90 is West; 270 is East.')
 @click.option('--folder', type=click.Path(
     exists=False, file_okay=False, dir_okay=True, resolve_path=True), default='.',
     help='Output folder for the two generated .sky files.')
 @click.option(
-    '--log-file', help='Optional log file to output the information about the two '
-    'generated sky files. By default the list will be printed out to stdout',
+    '--log-file', help='Optional log file to output the information about the '
+    'four generated sky files. By default the list will be printed out to stdout.',
     type=click.File('w'), default='-')
 def abnt_nbr_15575(wea, north, folder, log_file):
     """Generate four CIE skies for ABNT NBR 15575.
