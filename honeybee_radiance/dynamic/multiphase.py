e2d24e73e7cf3d3814261d129eb633d8882224d1
Chris Mackey <chris@ladybug.tools>
2024-11-14 09:18:33 -0800

    fix(multiphase): Refactor multiphase to avoid copy/pasted code
    This should enable us to use the same code in the CLI, in LBT-Grasshopper, and in the Rhino plugin.

--- honeybee_radiance/dynamic/multiphase.py
+++ honeybee_radiance/dynamic/multiphase.py
@@ -3,14 +3,20 @@
 from __future__ import division
 import os
 import math
+import json
 from collections import OrderedDict
 
 from ladybug_geometry.geometry3d.mesh import Mesh3D
-from honeybee_radiance_command.rfluxmtx import RfluxmtxOptions, Rfluxmtx
+from ladybug.futil import write_to_file_by_name
+from honeybee.boundarycondition import Outdoors
 from honeybee.typing import clean_rad_string
+from honeybee.config import folders as hb_folders
+from honeybee_radiance_command.oconv import Oconv
+from honeybee_radiance_command.rfluxmtx import RfluxmtxOptions, Rfluxmtx
 
 from honeybee_radiance.config import folders
 from honeybee_radiance.sensorgrid import SensorGrid
+from honeybee_radiance.lightsource.sky.skydome import SkyDome
 
 
 def _index_and_min(distance_matrix):
@@ -117,7 +123,7 @@ def aperture_view_factor(
     project_folder, apertures, size=0.2, ambient_division=1000,
     receiver='rflux_sky.sky', octree='scene.oct',
     calc_folder='aperture_grouping'
-    ):
+):
     """Calculates the view factor for each aperture by sensor points."""
 
     # Instantiate dictionary that will store the sensor count for each
@@ -181,7 +187,9 @@ def aperture_view_factor_postprocess(mtx_file, ap_dict, room_apertures, room_bas
             sensor_split = sensor.strip().split()
             if len(sensor_split) % 3 == 0:
                 one_channel = sensor_split[::3]
-                convert_to_vf = lambda x: float(x) / math.pi
+
+                def convert_to_vf(x):
+                    return float(x) / math.pi
                 view_factor.append(list(map(convert_to_vf, one_channel)))
 
     ap_view_factor = OrderedDict()
@@ -198,8 +206,7 @@ def aperture_view_factor_postprocess(mtx_file, ap_dict, room_apertures, room_bas
         ap_view_factor_mean[ap_id] = \
             [sum(sky_patch) / len(sky_patch) for sky_patch in ap_t]
 
-    if room_based:
-    # Restructure ap_view_factor_mean.
+    if room_based:  # Restructure ap_view_factor_mean.
         _ap_view_factor_mean = {}
         for room_id, data in room_apertures.items():
             _ap_view_factor_mean[room_id] = OrderedDict()
@@ -288,7 +295,7 @@ def cluster_orientation(room_apertures, apertures, room_based=True, vertical_tol
             for ap in data['apertures']:
                 # check if normal is already in list
                 n_bools = [ap.normal.is_equivalent(n, tolerance=0.01)
-                            for n in _normal_list]
+                           for n in _normal_list]
                 if not any(n_bools):
                     _normal_list.append(ap.normal)
                     # append empty list for new group
@@ -323,7 +330,7 @@ def cluster_orientation(room_apertures, apertures, room_based=True, vertical_tol
         for ap in apertures:
             # check if normal is already in list
             n_bools = [ap.normal.is_equivalent(n, tolerance=0.01)
-                        for n in _normal_list]
+                       for n in _normal_list]
             if not any(n_bools):
                 _normal_list.append(ap.normal)
                 # append empty list for new group
@@ -380,3 +387,171 @@ def cluster_output(ap_groups, room_apertures, room_based=True):
                 group_dict[ap_id] = group_name
 
     return group_names, group_dict
+
+
+def automatic_aperture_grouping(
+    model, octree=None, rflux_sky=None, size=0.2, threshold=0.001, ambient_division=1000,
+    room_based=True, view_factor_or_orientation=True,
+    vertical_tolerance=None, states=None, working_folder=None
+):
+    """Automatically calculate aperture groups for exterior apertures.
+
+    This function calculates view factor from apertures to sky patches (rfluxmtx). Each
+    aperture is represented by a sensor grid, and the view factor for the whole aperture
+    is the average of the grid. The apertures are grouped based on the threshold.
+
+    Args:
+        model: A Honeybee Model object to which aperture groups will be assigned.
+        octree: Optional path to octree file to be used for view factor calculation.
+            If None, the octree will be created from the model.
+        rflux_sky: Optional path to an rflux sky file. If None, the rflux sky
+            file will be auto-created.
+        size: Aperture grid size. A lower number will give a finer grid and more
+            accurate results but the calculation time will increase. (Default: 0.2).
+        threshold: A number that determines if two apertures/aperture groups can
+            be clustered. A lower number is more accurate but will also increase
+            the number of aperture groups. (Default: 0.001).
+        ambient_division: Number of ambient divisions (-ad) for view factor
+            calculation in rfluxmtx. Increasing the number will give more accurate
+            results but also increase the calculation time. (Default: 1000).
+        room_based: Boolean to note whether the apertures should be grouped
+            on a room basis. If grouped on a room basis apertures from different
+            room cannot be in the same group. (Default: False).
+        view_factor_or_orientation: Boolean to note whether the apertures should
+            be grouped by calculating view factors for the apertures to a
+            discretized sky (True) or simply by the normal orientation of the
+            apertures (False). (Default: False).
+        vertical_tolerance: A float value for vertical tolerance between two apertures.
+            If the vertical distance between two apertures is larger than this
+            tolerance the apertures cannot be grouped. If None, the vertical
+            grouping will be skipped. (Default: None).
+        states: An optional list of Honeybee State objects to be applied to all
+            the generated groups. These states should be ordered based on how
+            they will be switched on. The first state is the default state and,
+            typically, higher states are more shaded. If the objects in the group
+            have no states, the modifiers already assigned the apertures will
+            be used for all states.
+        working_folder: Path to a folder into which the files be written. If None,
+            the files will be written into a folder called aperture_groups
+            within the default simulation folder.
+
+    Returns:
+        A Model with Aperture groups automatically assigned.
+    """
+    # serialize the model, set the output folder, and process simpler attributes
+    if working_folder is None:
+        working_folder = os.path.join(hb_folders.default_simulation_folder,
+                                      'aperture_groups')
+    if not os.path.isdir(working_folder):
+        os.makedirs(working_folder)
+    view_factor = view_factor_or_orientation
+
+    # Get all room-based apertures with Outdoors boundary condition
+    apertures = []
+    room_apertures = {}
+    for room in model.rooms:
+        for face in room.faces:
+            for ap in face.apertures:
+                if isinstance(ap.boundary_condition, Outdoors):
+                    apertures.append(ap)
+                    if room.identifier not in room_apertures:
+                        room_apertures[room.identifier] = {}
+                    if 'apertures' not in room_apertures[room.identifier]:
+                        room_apertures[room.identifier]['apertures'] = \
+                            [ap]
+                    else:
+                        room_apertures[room.identifier]['apertures'].append(ap)
+                    if 'display_name' not in room_apertures[room.identifier]:
+                        room_apertures[room.identifier]['display_name'] = \
+                            room.display_name
+
+    assert len(apertures) != 0, \
+        'Found no Honeybee Apertures. There should at least be one Aperture ' \
+        'in the model to compute aperture groups.'
+
+    if view_factor:
+        if not octree:
+            # write octree
+            model_content, modifier_content = model.to.rad(model, minimal=True)
+            scene_file, mat_file = 'scene.rad', 'scene.mat'
+            write_to_file_by_name(working_folder, scene_file, model_content)
+            write_to_file_by_name(working_folder, mat_file, modifier_content)
+            octree = 'scene.oct'
+            oconv = Oconv(inputs=[mat_file, scene_file], output=octree)
+            oconv.options.f = True
+
+            # run Oconv command
+            env = None
+            if folders.env != {}:
+                env = folders.env
+            env = dict(os.environ, **env) if env else None
+            oconv.run(env, cwd=working_folder)
+
+        if not rflux_sky:
+            rflux_sky = SkyDome()
+            rflux_sky = rflux_sky.to_file(working_folder, name='rflux_sky.sky')
+
+        # Calculate view factor.
+        mtx_file, ap_dict = aperture_view_factor(
+            working_folder, apertures, size=size, ambient_division=ambient_division,
+            receiver=rflux_sky, octree=octree, calc_folder=working_folder
+        )
+        rmse = aperture_view_factor_postprocess(
+            mtx_file, ap_dict, room_apertures, room_based
+        )
+
+    # cluster apertures into groups
+    if view_factor:
+        ap_groups = cluster_view_factor(
+            rmse, room_apertures, apertures, threshold, room_based, vertical_tolerance)
+    else:
+        ap_groups = cluster_orientation(
+            room_apertures, apertures, room_based, vertical_tolerance
+        )
+
+    # process clusters
+    group_names, group_dict = \
+        cluster_output(ap_groups, room_apertures, room_based)
+
+    # Write aperture groups to JSON file.
+    dyn_gr = os.path.join(working_folder, 'aperture_groups.json')
+    with open(dyn_gr, 'w') as fp:
+        json.dump(group_names, fp, indent=2)
+
+    # Write dynamic group identifiers to JSON file.
+    dyn_gr_ids = os.path.join(working_folder, 'dynamic_group_identifiers.json')
+    with open(dyn_gr_ids, 'w') as fp:
+        json.dump(group_dict, fp, indent=2)
+
+    # assign dynamic group identifiers for each aperture
+    group_ap_dict = {}
+    for room in model.rooms:
+        for face in room.faces:
+            for ap in face.apertures:
+                if isinstance(ap.boundary_condition, Outdoors):
+                    dyn_group_id = group_dict[ap.identifier]
+                    ap.properties.radiance.dynamic_group_identifier = \
+                        dyn_group_id
+                    try:
+                        group_ap_dict[dyn_group_id].append(ap)
+                    except KeyError:
+                        group_ap_dict[dyn_group_id] = [ap]
+
+    # assign any states if they are connected
+    if states is not None and len(states) != 0:
+        for group_aps in group_ap_dict.values():
+            # assign states (including shades) to the first aperture
+            group_aps[0].properties.radiance.states = \
+                [state.duplicate() for state in states]
+            # remove shades from following apertures to ensure they aren't double-counted
+            states_wo_shades = []
+            for state in states:
+                new_state = state.duplicate()
+                new_state.remove_shades()
+                states_wo_shades.append(new_state)
+            for ap in group_aps[1:]:
+                ap.properties.radiance.states = \
+                    [state.duplicate() for state in states_wo_shades]
+
+    # return the model
+    return model
